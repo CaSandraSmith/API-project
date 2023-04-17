368de6d4a90db7dc3daae9be506d64ea13843d06
CaSandraSmith <casresm3@gmail.com>
2023-04-17 14:16:24 -0700

    Spot Table and Model created

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -5,6 +5,12 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       // define association here
+      User.hasMany(
+        models.Spot,
+        {
+          foreignKey: 'ownerId'
+        }
+      )
     }
   };
 
