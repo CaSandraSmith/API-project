557e8bce26f2a4f3ffc394082121c85ed575489e
CaSandraSmith <casresm3@gmail.com>
2023-04-17 19:14:37 -0700

    Fixed issue with naming on spot image seeder file.

--- backend/db/seeders/20230418010542-create-spot-image-seeds.js
+++ backend/db/seeders/20230418010542-create-spot-image-seeds.js
@@ -12,17 +12,17 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert(options, [
       {
-        spotid: 1,
+        spotId: 1,
         url: 'https://untappedcities.com/wp-content/uploads/2019/10/Empire-State-Building-102nd-Floor-Observatory-Floor-to-Ceiling-Windows-360-Degree-View-NYC-007.jpg',
         preview: true
       },
       {
-        spotid: 2,
+        spotId: 2,
         url: 'https://static.wikia.nocookie.net/harrypotter/images/f/fc/GrimmauldPlace_WB_F5_FrontOfGrimmualdPlace_Image_100615_Land.jpg/revision/latest?cb=20161017204708',
         preview: true
       },
       {
-        spotid: 3,
+        spotId: 3,
         url: 'https://www.housedigest.com/img/gallery/heres-where-you-can-visit-the-cullens-house-from-twilight/l-intro-1651765830.jpg',
         preview: true
       }
