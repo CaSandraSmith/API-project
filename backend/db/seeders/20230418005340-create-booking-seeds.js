a665d4f1899216bb864fc71a79c846dde0d033ca
CaSandraSmith <casresm3@gmail.com>
2023-04-17 18:03:46 -0700

    Booking seed data created.

---
+++ backend/db/seeders/20230418005340-create-booking-seeds.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+options.tableName = 'Bookings'
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        userId: 3,
+        startDate: '2023-01-17',
+        endDate: '2023-01-25'
+      },
+      {
+        spotId: 2,
+        userId: 1,
+        startDate: '2023-03-01',
+        endDate: '2023-03-06'
+      },
+      {
+        spotId: 3,
+        userId: 1,
+        startDate: '2023-02-22',
+        endDate: '2023-02-28'
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete(options)
+  }
+};
