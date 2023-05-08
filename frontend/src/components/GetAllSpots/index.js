52dcacdd2fede6ab466e09a84c95506ef3599d86
CaSandraSmith <casresm3@gmail.com>
2023-05-08 13:39:36 -0700

    Get all spots thunk action creator created and added to spot reducer

--- frontend/src/components/GetAllSpots/index.js
+++ frontend/src/components/GetAllSpots/index.js
@@ -1,5 +1,21 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { loadSpots } from '../../store/spots';
+
 export default function GetAllSpots() {
+    const dispatch = useDispatch();
+    const spots = useSelector((state) => state.spots.allSpots);
+    const allSpots = Object.values(spots);
+    console.log(allSpots)
+    useEffect(() => {
+        dispatch(loadSpots())
+    }, [dispatch])
+
     return (
-        <p>hello</p>
+        <ul>
+            {allSpots.map((spot) => {
+                <li>{spot.name}</li>
+            })}
+        </ul>
     )
 }
---
+++ frontend/src/store/spots.js
@@ -0,0 +1,35 @@
+//set type in constant variable to avoid typing errors
+const GET_SPOTS = "spots/getAllSpots";
+
+//action creators
+const loadAllSpots = (spots) => ({
+    type: GET_SPOTS,
+    spots: spots.Spots
+})
+
+
+//thunk action creators
+export const loadSpots = () => async (dispatch) => {
+    const response = await fetch('/api/spots')
+    const spots = await response.json()
+    return dispatch(loadAllSpots(spots))
+}
+
+
+const initialState = {};
+
+const spotReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case GET_SPOTS:
+        newState = {}
+        action.spots.forEach(spot => {
+            newState[spot.id] = spot
+        });
+        return {...state, allSpots: {...newState}}
+    default:
+      return state;
+  }
+};
+
+export default spotReducer;
