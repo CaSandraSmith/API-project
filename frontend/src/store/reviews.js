0d7866ddb9770f0328265a28fc11d83f71c8b669
CaSandraSmith <casresm3@gmail.com>
2023-05-11 08:09:40 -0700

    Build action to get the reviews for a spot

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -1,9 +1,27 @@
 import { csrfFetch } from "./csrf";
 
+const SPOT_REVIEWS = "reviews/getSpotReviews"
+
+const getReviews = (reviews) => ({
+    type: SPOT_REVIEWS,
+    reviews: reviews.Reviews
+})
+
+const getReviewsBySpotId = (spotId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/spots/${spotId}/reviews`)
+    const reviews = await res.json()
+    dispatch(getReviews(reviews))
+}
 const initialState = {spot: {}, user: {}}
 
 const reviewReducer = (state = initialState, action) => {
     switch (action.type) {
+        case SPOT_REVIEWS:
+            let newState = {};
+            action.reviews.forEach(rev => {
+                newState[rev.id] = rev
+            });
+            return {...state, spot: {...newState}}
         default:
             return state
     }
