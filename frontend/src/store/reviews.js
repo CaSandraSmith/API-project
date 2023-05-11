03e000de005421ec2185e4d07e5665a9951a7d0a
CaSandraSmith <casresm3@gmail.com>
2023-05-11 07:50:14 -0700

    Built review reducer

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,12 @@
+import { csrfFetch } from "./csrf";
+
+const initialState = {spot: {}, user: {}}
+
+const reviewReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+};
+
+export default reviewReducer;
