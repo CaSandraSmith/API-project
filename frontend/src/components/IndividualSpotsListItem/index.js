3872ff86b4ee9d69b55036d10861e6338a9c4ca6
CaSandraSmith <casresm3@gmail.com>
2023-05-14 16:59:14 -0700

    Home page layput complete except tooltip

--- frontend/src/components/IndividualSpotsListItem/index.js
+++ frontend/src/components/IndividualSpotsListItem/index.js
@@ -10,17 +10,30 @@ export default function IndividualSpotListItem({ spot }) {
         history.push(`/spots/${spot.id}`)
     }
     return (
-        <div onClick={movePage} className={`all-spots-page-spot-wrapper ${currentSpot}`} role="tooltip" aria-describedby={spot.name} >
-            {spot.previewImage ? <img src={spot.previewImage} alt={spot.name} /> : <p>No preview Image</p>}
-            <p>
-                {spot.city}, {spot.state}
-            </p>
-            <p>
-                ${spot.price} per night
-            </p>
-            <p>
-                <i className="fa-solid fa-star"></i>{spot.avgRating}
-            </p>
+        <div onClick={movePage} className={`all-spots-page-spot-wrapper ${currentSpot}`} data-title={spot.name}>
+            <img src={spot.previewImage} alt={spot.name} />
+            <div className="spot-info-wrapper">
+                <div className="get-all-spots-location-stars">
+                    <p>
+                        {spot.city}, {spot.state}
+                    </p>
+                    <div>
+                        <div>
+                            {spot.avgRating ?
+                                <p className="all-spots-bold">
+                                    <i className="fa-solid fa-star"></i> {spot.avgRating.toFixed(1)}
+                                </p> :
+                                <p>
+                                    <i className="fa-solid fa-star"></i> New
+                                </p>
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p><span className="all-spots-bold">${spot.price}</span> night</p>
+                </div>
+            </div>
         </div>
     )
 }
