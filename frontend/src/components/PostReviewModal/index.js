810d87caf2978def7ee5bdd2923dad16aa046bdc
CaSandraSmith <casresm3@gmail.com>
2023-05-15 00:01:05 -0700

    Css for review modal completed

--- frontend/src/components/PostReviewModal/index.js
+++ frontend/src/components/PostReviewModal/index.js
@@ -19,10 +19,10 @@ export default function PostReviewModal({ spot }) {
         return false
     }
 
-    async function handleSubmit (e) {
+    async function handleSubmit(e) {
         setErrors({})
         e.preventDefault()
-        let reviewData = {stars, review}
+        let reviewData = { stars, review }
         let reviewResponse = await dispatch(createReview(reviewData, spot.id))
 
         if (reviewResponse.errors) setErrors(reviewResponse.errors)
@@ -32,39 +32,41 @@ export default function PostReviewModal({ spot }) {
     }
 
     return (
-        <div>
+        <div className="post-review-modal-wrapper">
             <h1>How was your stay?</h1>
             {errors.review ? <h2>{errors.review}</h2> : null}
             {errors.stars ? <h2>{errors.stars}</h2> : null}
-            <form>
-                <label>
+            <form className="review-form">
+                <label className="review-input-area">
                     <textarea
                         value={review}
                         onChange={(e) => setReview(e.target.value)}
                         placeholder="Leave your review here..."
                     />
                 </label>
-                <div>
-                    <div >
-                        <i className={`fa-star ${stars >= 1 || activeStars >= 1 ? "fa-solid" : "fa-regular"}`} onClick={() => setStars(1)} onMouseEnter={() => setActiveStars(1)} ></i>
-                    </div>
-                    <div>
-                        <i className={`fa-star ${stars >= 2 || activeStars >= 2 ? "fa-solid" : "fa-regular"}` } onClick={() => setStars(2)} onMouseEnter={() => setActiveStars(2)}></i>
-                    </div>
-                    <div>
-                        <i className={`fa-star ${stars >= 3 || activeStars >= 3 ? "fa-solid" : "fa-regular"}`} onClick={() => setStars(3)} onMouseEnter={() => setActiveStars(3)}></i>
-                    </div>
-                    <div>
-                        <i className={`fa-star ${stars >= 4 || activeStars >= 4 ? "fa-solid" : "fa-regular"}`} onClick={() => setStars(4)} onMouseEnter={() => setActiveStars(4)}></i>
-                    </div>
-                    <div >
-                        <i className={`fa-star ${stars === 5 || activeStars >= 5 ? "fa-solid" : "fa-regular"}`} onClick={() => setStars(5)} onMouseEnter={() => setActiveStars(5)}></i>
+                <div className="rating-stars-input-wrapper">
+                    <div className="rating-stars-input">
+                        <div>
+                            <i className={`fa-star ${stars >= 1 || activeStars >= 1 ? "fa-solid" : "fa-regular"}`} onClick={() => setStars(1)} onMouseEnter={() => setActiveStars(1)} ></i>
+                        </div>
+                        <div>
+                            <i className={`fa-star ${stars >= 2 || activeStars >= 2 ? "fa-solid" : "fa-regular"}`} onClick={() => setStars(2)} onMouseEnter={() => setActiveStars(2)}></i>
+                        </div>
+                        <div>
+                            <i className={`fa-star ${stars >= 3 || activeStars >= 3 ? "fa-solid" : "fa-regular"}`} onClick={() => setStars(3)} onMouseEnter={() => setActiveStars(3)}></i>
+                        </div>
+                        <div>
+                            <i className={`fa-star ${stars >= 4 || activeStars >= 4 ? "fa-solid" : "fa-regular"}`} onClick={() => setStars(4)} onMouseEnter={() => setActiveStars(4)}></i>
+                        </div>
+                        <div >
+                            <i className={`fa-star ${stars === 5 || activeStars >= 5 ? "fa-solid" : "fa-regular"}`} onClick={() => setStars(5)} onMouseEnter={() => setActiveStars(5)}></i>
+                        </div>
                     </div>
                     <div>
                         <p>Stars</p>
                     </div>
                 </div>
-                <button onClick={handleSubmit} disabled={checkReview()}>Submit Your Review</button>
+                <button className="submit-review-button" onClick={handleSubmit} disabled={checkReview()}>Submit Your Review</button>
             </form>
         </div>
     )
