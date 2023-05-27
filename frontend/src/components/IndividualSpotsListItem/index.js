3a69faa81ba7da76ec2f7f6e5a4ede4962db3afe
CaSandraSmith <casresm3@gmail.com>
2023-05-27 14:44:17 -0700

    Made adjustments to homepage to allow text wrapping when city name and state are long

--- frontend/src/components/IndividualSpotsListItem/index.js
+++ frontend/src/components/IndividualSpotsListItem/index.js
@@ -10,26 +10,26 @@ export default function IndividualSpotListItem({ spot }) {
     return (
         <div onClick={movePage} className={`all-spots-page-spot-wrapper`} title={spot.name}>
             <img src={spot.previewImage} alt={spot.name} />
-            <div className="spot-info-wrapper">
-                <div className="get-all-spots-location-stars">
-                    <p className="all-spots-page-bold">
-                        {spot.city}, {spot.state}
-                    </p>
+            <div className="all-spot-info-wrapper">
+                <div className="get-all-spots-location-price">
                     <div>
-                        <div>
-                            {spot.avgRating ?
-                                <p>
-                                    <i className="fa-solid fa-star"></i> {spot.avgRating.toFixed(1)}
-                                </p> :
-                                <p>
-                                    <i className="fa-solid fa-star"></i> New
-                                </p>
-                            }
-                        </div>
+                        <p className="all-spots-page-bold all-spots-location">
+                            {spot.city}, {spot.state}
+                        </p>
+                    </div>
+                    <div>
+                        <p><span className="all-spots-page-bold">${spot.price}</span> night</p>
                     </div>
                 </div>
-                <div>
-                    <p><span className="all-spots-page-bold">${spot.price}</span> night</p>
+                <div className="all-spots-stars">
+                    {spot.avgRating ?
+                        <p>
+                            <i className="fa-solid fa-star"></i> {spot.avgRating.toFixed(1)}
+                        </p> :
+                        <p>
+                            <i className="fa-solid fa-star"></i> New
+                        </p>
+                    }
                 </div>
             </div>
         </div>
