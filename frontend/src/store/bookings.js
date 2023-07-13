48e02216b42755d9be3193d88a5de7b8f4b2d626
CaSandraSmith <casresm3@gmail.com>
2023-07-13 18:52:10 -0400

    Created action to create booking

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -1,7 +1,44 @@
+import { csrfFetch } from "./csrf";
+
+const CREATE_BOOKING = "bookings/create"
+
+const createNewBooking = (booking) => ({
+    type: CREATE_BOOKING,
+    booking
+})
+
+export const createBooking = (spotId, booking) => async (dispatch) => {
+    let res = await csrfFetch(`/api/spots/${spotId}/bookings`,  {
+        method: 'POST',
+        body: JSON.stringify(booking)
+    }).catch(async (errs) => {
+        const err = await errs.json();
+        return err
+    })
+
+    if (res.ok) {
+        let newBooking = await res.json()
+        dispatch(createNewBooking(newBooking))
+        return newBooking
+    } else {
+        return res
+    }
+}
+
 const initialState = {user: {}, spot: {}}
 
 const bookingsReducer = (state = initialState, action) => {
     switch (action.type) {
+        case CREATE_BOOKING:
+            return {...state, 
+                user: {
+                        ...state.user,
+                        [action.booking.id]: action.booking
+                },
+                spot: {...state.spot,
+                    [action.booking.id]: action.booking
+                }
+            }
         default:
             return state
     }
