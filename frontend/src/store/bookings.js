ee0f8c6ce2201c4a111fb21b82224f54c12bebe1
CaSandraSmith <casresm3@gmail.com>
2023-07-13 19:11:07 -0400

    Created action to get all of the bookings of the current user

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -1,10 +1,16 @@
 import { csrfFetch } from "./csrf";
 
 const CREATE_BOOKING = "bookings/create"
+const GET_CURRENT_USER_BOOKINGS = "bookings/currentUser"
 
 const createNewBooking = (booking) => ({
     type: CREATE_BOOKING,
     booking
+})
+
+const allCurrentUserBookings = (bookings) => ({
+    type: GET_CURRENT_USER_BOOKINGS,
+    bookings: bookings.Bookings
 })
 
 export const createBooking = (spotId, booking) => async (dispatch) => {
@@ -15,7 +21,7 @@ export const createBooking = (spotId, booking) => async (dispatch) => {
         const err = await errs.json();
         return err
     })
-
+    
     if (res.ok) {
         let newBooking = await res.json()
         dispatch(createNewBooking(newBooking))
@@ -25,10 +31,36 @@ export const createBooking = (spotId, booking) => async (dispatch) => {
     }
 }
 
+export const getCurrentUserBookings = () => async(dispatch) => {
+    let res = await csrfFetch("/api/bookings/current").catch(async (errs) => {
+        const err = await errs.json();
+        return err
+    })
+    
+    if (res.ok) {
+        let bookings = await res.json()
+        dispatch(allCurrentUserBookings(bookings))
+        return bookings
+    } else {
+        return res
+    }
+
+}
+
 const initialState = {user: {}, spot: {}}
 
 const bookingsReducer = (state = initialState, action) => {
     switch (action.type) {
+        case GET_CURRENT_USER_BOOKINGS:
+            let userBookings = {}
+            for (let booking in action.bookings) {
+                userBookings[booking.id] = booking
+            }
+            return {
+                ...state,
+                user: {...userBookings},
+                spot: {...state.spot}
+            }
         case CREATE_BOOKING:
             return {...state, 
                 user: {
