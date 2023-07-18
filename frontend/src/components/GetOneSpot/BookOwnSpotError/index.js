7ad12cb3763e75bcd934575ea86f47bc692b6c0d
CaSandraSmith <casresm3@gmail.com>
2023-07-17 20:06:47 -0400

    Finished styling for user attempting to book thier own spot

--- frontend/src/components/GetOneSpot/BookOwnSpotError/index.js
+++ frontend/src/components/GetOneSpot/BookOwnSpotError/index.js
@@ -1,9 +1,19 @@
+import { useHistory } from "react-router-dom"
+import { useModal } from "../../../context/Modal"
+
 export default function BookOwnSpotError() {
+    const { closeModal } = useModal()
+    const history = useHistory()
+
+    let homeClick = () => {
+        history.push("/")
+        closeModal()
+    }
     return (
-        <div>
-            <h1>You can't book your own spot!</h1>
-            <h2>Check out some of our other spots today</h2>
-            <button>Home</button>
+        <div className="book-own-spot-error-wrapper">
+            <p>Oh no ... you can't book your own spot</p>
+            <p>Check out some of our other spots today</p>
+            <button onClick={homeClick}>Home</button>
         </div>
     )
 }
