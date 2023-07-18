039a36a8c5a5c29b31f7e29db25da6fd661d5f85
CaSandraSmith <casresm3@gmail.com>
2023-07-18 02:55:33 -0400

    Added about links

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -19,6 +19,16 @@ function Navigation({ isLoaded }) {
           </div>
         </NavLink>
       </div>
+      <div>
+        <a target='_blank' href="https://github.com/CaSandraSmith/characterBnB" className="about-links">
+          <i className="fa-brands fa-github"></i> Github
+        </a>
+      </div>
+      <div>
+        <a target='_blank' href="https://www.linkedin.com/in/casandra-smith/" className="about-links">
+          <i class="fa-brands fa-linkedin"></i> Linkedin
+        </a>
+      </div>
       {isLoaded && (
         <div>
           <ProfileButton user={sessionUser} />
